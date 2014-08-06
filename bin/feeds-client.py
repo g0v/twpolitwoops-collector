0b68b79e3e9cdacb42a78f1f44c44203d311e6d4
lanfon72 <lanfon72@gmail.com>
2014-08-06 17:26:42 +0800

    update.

---
+++ bin/feeds-client.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+
+feeds-client.py
+created by lanfon on 2014, 
+it's modify from:
+
+tweets-client.py
+
+Created by Breyten Ernsting on 2010-05-30.
+Copyright (c) 2010 Back-End-Front Web Development. All rights reserved.
+"""
+
+import os
+import sys
+import argparse
+import signal
+import ConfigParser
+
+import socket
+# disable buffering
+socket._fileobject.default_bufsize = 0
+
+import httplib
+httplib.HTTPConnection.debuglevel = 1
+
+import anyjson
+import logbook
+
+# this is for consuming the streaming API
+import tweepy
+import tweetsclient
+import politwoops
+import streaming
+
+_script_ = (os.path.basename(__file__)
+            if __name__ == "__main__"
+            else __name__)
+log = logbook.Logger(_script_)
+
+class DataRecord(object):
+    def __init__(self, *args, **kwargs):
+        object.__setattr__(self, '_dict', {})
+        self._dict.update(((arg, None) for arg in args))
+        self._dict.update(kwargs)
+
+    def __getattr__(self, attr):
+        if attr not in self._dict:
+            raise AttributeError("{cls!r} has no attribute {attr!r}".format(cls=self.__class__.__name__,
+                                                                        attr=attr))
+        return self._dict[attr]
+
+    def __setattr__(self, attr, value):
+        raise AttributeError("All attributes of DataRecord objects are read-only")
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def dict_mget(thedict, keylist, default=None):
+    result = reduce(lambda d, k: None if d is None else d.get(k), keylist, thedict)
+    return result if result is not None else default
+
+class FeedListener(streaming.StreamListener):
+    def __init__(self, queue, *args, **kwargs):
+        super(FeedListener, self).__init__(*args, **kwargs)
+        self.queue = queue
+
+    def on_data(self, data):
+        #log.notice('caught a feed')
+        feed = data
+        #feed = anyjson.deserialize(data)
+        if feed.has_key('from'):
+            log.notice(u'Queued feed for user {0}/{1}'.format(dict_mget(feed,['from','id']),dict_mget(feed,['from','name'])))
+        """    
+        try:
+            
+            tweet = anyjson.deserialize(data)
+            self.queue.put(anyjson.serialize(tweet))
+            if tweet.has_key('delete'):
+                status = dict_mget(tweet, ['delete', 'status'])
+                if status is not None:
+                    log.notice(u"Queued delete notification for user {0} for tweet {1}".format(status.get('user_id_str'), status.get('id_str')))
+
+            elif tweet.has_key('user'):
+                log.notice(u"Queued tweet for user {0}/{1}".format(dict_mget(tweet, ['user', 'screen_name']), dict_mget(tweet, ['user', 'id_str'])))
+
+            else:
+                log.notice(u"Queued tweet: {0}".format(tweet))
+                
+        except Exception as e:
+            log.error(u"TweetListener.on_data() caught exception: {0}".format(unicode(e)))
+            return False  # Closes connection, stops streaming
+        """
+
+    def on_timeout(self):
+        log.error(u"TweetListener connection timed out.")
+
+    def on_error(self, status_code):
+        log.error(u"TweetListener got bad status code: {0}".format(status_code))
+
+class FeedStreamClient(object):
+    def __init__(self):
+        self.config = tweetsclient.Config().get()
+        consumer_key = self.config.get('tweets-client', 'consumer_key')
+        consumer_secret = self.config.get('tweets-client', 'consumer_secret')
+        access_token = self.config.get('tweets-client', 'access_token')
+        access_token_secret = self.config.get('tweets-client', 'access_token_secret')
+        log.debug("Consumer credentials: {key}, {secret}",
+                  key=consumer_key,
+                  secret=consumer_secret)
+        log.debug("Access credentials: {token}, {secret}",
+                  token=access_token,
+                  secret=access_token_secret)
+        self.twitter_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.twitter_auth.set_access_token(access_token, access_token_secret)
+        try:
+            username = self.twitter_auth.get_username()
+            log.notice("Authenticated as {user}".format(user=username))
+        except tweepy.error.TweepError as e:
+            log.error(unicode(e))
+
+    def get_config_default(self, section, key, default = None):
+        try:
+            return self.config.get(section, key)
+        except ConfigParser.NoOptionError:
+            return default
+
+    def load_plugin(self, plugin_module, plugin_class):
+        pluginModule = __import__(plugin_module)
+        components = plugin_module.split('.')
+        for comp in components[1:]:
+            pluginModule = getattr(pluginModule, comp)
+        pluginClass = getattr(pluginModule, plugin_class)
+        return pluginClass
+
+    def init_beanstalk(self):
+        tweets_tube = self.config.get('beanstalk', 'tweets_tube')
+
+        log.info("Initiating beanstalk connection. Queueing tweets to {use}...", use=tweets_tube)
+
+        self.beanstalk = politwoops.utils.beanstalk(host=self.config.get('beanstalk', 'host'),
+                                                    port=int(self.config.get('beanstalk', 'port')),
+                                                    watch=None,
+                                                    use=tweets_tube)
+
+    def stream_forever(self):
+        """
+        track_module = self.get_config_default('tweets-client', 'track-module', 'tweetsclient.config_track')
+        track_class = self.get_config_default('tweets-client', 'track-class', 'ConfigTrackPlugin')
+        log.debug("Loading track plugin: {module} - {klass}",
+                  module=track_module, klass=track_class)
+
+        pluginClass = self.load_plugin(track_module, track_class)
+        self.track = pluginClass()
+        stream_type = self.track.get_type()
+        log.debug("Initializing a {0} stream of tweets.", stream_type)
+        track_items = self.track.get_items()
+        log.debug(str(track_items))
+
+        stream = None
+        if stream_type == 'users':
+            tweet_listener = TweetListener(self.beanstalk)
+            stream = tweepy.Stream(self.twitter_auth, tweet_listener)
+            stream.filter(follow=track_items)
+        elif stream_type == 'words':
+            raise Exception('The words stream type is no longer supported.')
+        else:
+            raise Exception('Unrecognized stream type: {0}'.format(stream_type))
+        """
+        feed_listener = FeedListener(self.beanstalk)
+        facebook_token = 'CAAE72Fovp7YBAAv5gSnUG8JcuVxstXtriZBrwmdGUP4YHuZCA5yZAavdPADEQmFkgWxp2kzFaRMrO9j0pUCiyFu361Yr8koBClNO16ybXaTDogty3u8YqUZAxvSPrzCbfa5QpjZA71qAdTIB9qJBJpuWAZCtTDL2twAHNyzWMHcNGFqJXln79OOo7D3qBA7PbS9KXXdNgZA5H9qyUg4hjwqFhLhfRk99ZA8ZD'
+        stream = streaming.Stream(auth=facebook_token, listener=feed_listener)
+        stream.filter()
+
+    def run(self):
+        self.init_beanstalk()
+        with politwoops.utils.Heart() as heart:
+            politwoops.utils.start_heartbeat_thread(heart)
+            politwoops.utils.start_watchdog_thread(heart)
+            self.stream_forever()
+
+        self.beanstalk.disconnect()
+        return 0
+
+def main(args):
+    signal.signal(signal.SIGHUP, politwoops.utils.restart_process)
+
+    log_handler = politwoops.utils.configure_log_handler(_script_, args.loglevel, args.output)
+    with logbook.NullHandler():
+        with log_handler.applicationbound():
+            log.debug("Starting tweets-client.py")
+            try:
+                app = FeedStreamClient()
+                if args.authtest:
+                    return
+                if args.restart:
+                    return politwoops.utils.run_with_restart(app.run)
+                else:
+                    return app.run()
+            except KeyboardInterrupt:
+                log.notice("Killed by CTRL-C")
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser(description=__doc__)
+    args_parser.add_argument('--loglevel', metavar='LEVEL', type=str,
+                             help='Logging level (default: notice)',
+                             default='notice',
+                             choices=('debug', 'info', 'notice', 'warning',
+                                      'error', 'critical'))
+    args_parser.add_argument('--output', metavar='DEST', type=str,
+                             default='-',
+                             help='Destination for log output (-, syslog, or filename)')
+    args_parser.add_argument('--restart', default=False, action='store_true',
+                             help='Restart when an error cannot be handled.')
+    args_parser.add_argument('--authtest', default=False, action='store_true',
+                             help='Authenticate against Twitter and exit.')
+    args = args_parser.parse_args()
+    sys.exit(main(args))
---
+++ lib/streaming.py
@@ -0,0 +1,109 @@
+import time
+import facebook
+import logging
+
+API_VERSION = 'v2.0'
+
+class StreamListener(object):
+
+    def __init__(self):
+        logging.debug('listener init')
+    def on_connect(self):
+        pass
+    def on_data(self, raw_data):
+        logging.error(raw_data.keys())
+        # test for print out.
+
+class Stream(object):
+    fb_api = None
+    def __init__(self, auth, listener, **options):
+        self.auth = auth
+        self.listener = listener
+        self.running = False
+        self.timeout = options.get('timeout')
+        self.retry_count = options.get("retry_count")
+        # values according to https://dev.twitter.com/docs/streaming-apis/connecting#Reconnecting
+        self.retry_time_start = options.get("retry_time", 60)
+        #self.retry_420_start = options.get("retry_420", 60.0)
+        self.retry_time_cap = options.get("retry_time_cap", 320.0)
+        self.snooze_time_step = options.get("snooze_time", 0.25)
+        self.snooze_time_cap = options.get("snooze_time_cap", 16)
+        self.buffer_size = options.get("buffer_size",  1500)
+
+        #self.api = API()
+        #self.session = requests.Session()
+        #self.session.headers = options.get("headers") or {}
+        #self.session.params = None
+        self.body = None
+        self.retry_time = self.retry_time_start
+        self.snooze_time = self.snooze_time_step
+
+    def _run(self):
+        # Authenticate
+        args = { 'since' : self.start_time }
+        resp = None
+        exception = None
+        while self.running:
+            try:
+            # get fb.request feeds + token
+            # sleep 5mins
+                resp = self.fb_api.request(self.url, args)
+                self.listener.on_connect()
+                logging.error('running, if on_data, retry in ' + `self.retry_time` )
+                self._read_loop(resp['data'])
+                time.sleep(self.retry_time)
+                start_time = int( time.time() )
+            except Exception as exception:
+                # any exception is fatal, so kill loop.
+                break
+
+        # clean up
+        self.running = False
+
+        if exception:
+            # call a hanlder first so that the exception can be logged.
+            #self.listener.on_exception(exception)
+            raise
+
+    def _data(self, data):
+        if self.listener.on_data(data) is False:
+            self.running = False
+
+    def _read_loop(self, resp):
+        # prase feed in feeds.
+        while resp:
+                feed = resp.pop()
+                self._data(feed)
+
+
+    def _start(self, async):
+        self.running = True
+        if async:
+            self._thread = Thread(target=self._run)
+            self.thread.start()
+        else:
+            self._run()
+
+    def on_closed(self, resp):
+        #
+        pass
+    def userstream(self,**args):
+        pass
+
+    def filter(self,follow=None, async=False):
+        if self.running:
+            pass  # *** there should raise ERROR like already connected. *** #
+        self.url = '/%s/me/home' % API_VERSION
+        self.fb_api = facebook.GraphAPI(access_token = self.auth)
+        self.start_time = int( time.time() )
+        self._start(async)
+
+    def disconnect(self):
+        if self.running is False:
+            return
+        self.running = False
+
+    #firehose
+    #retweet
+    #sample
+    #sitestream
